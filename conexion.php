f8560310e38c189efe6eed0356d414670794528e
Rodrigo Bolaños <rodrigobp@unicauca.edu.co>
2020-06-24 15:57:12 -0500

    Añadida conexión, formulario y tabla

---
+++ conexion.php
@@ -0,0 +1,6 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "CRUD") or die( "No se pudo conectar con la base de datos");
+/*$mysqli = new mysqli("localhost", "root", "", "CRUD");
+if ($mysqli->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}*/
